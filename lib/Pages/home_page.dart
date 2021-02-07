79ab9467935d9fa2b8395d35aec1e6e95272ee4a
DhruvDalal <dhruvdalal442002@gmail.com>
2021-02-07 16:03:07 +0530

    day 3 completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class HomePage extends StatelessWidget {
   final int days = 25;
   String name = "Dhruv";
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/Pages/home_page.dart';
+import 'package:flutter_catalog/Pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
   double rate = 3.14;
   bool isMale = true;
@@ -12,7 +14,21 @@ class MyApp extends StatelessWidget {
   var day = "Tuesday";
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage(),
-    debugShowCheckedModeBanner: false,);
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primaryColor: Colors.deepPurple),
+      // themeMode: ThemeMode.dark,
+      // darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context)=>LoginPage(),
+        "/home":(context)=>HomePage(),
+        "/login":(context)=>LoginPage(),
+      },
+    );
   }
 }
+
+
