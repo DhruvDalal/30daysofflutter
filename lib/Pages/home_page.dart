3dbd85b2078107eee08de44b163a861cf1231054
DhruvDalal <dhruvdalal442002@gmail.com>
2021-04-10 23:38:45 +0530

    day12 completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 // ignore: must_be_immutable
 class HomePage extends StatelessWidget {
@@ -7,15 +9,21 @@ class HomePage extends StatelessWidget {
   String name = "Dhruv";
   @override
   Widget build(BuildContext context) {
+    final dumyList = List.generate(20, (index) => catalogModel.items[0]);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App"),
-      ),
-      drawer: MyDrawer(),
-      body: Container(
-        decoration: BoxDecoration(color: Colors.white),
-        child: Center(child: Text("Welcome to $days Days of Flutter, $name")),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Catalog App"),
+        ),
+        drawer: MyDrawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dumyList[index],
+              );
+            },
+          ),
+        ));
   }
 }
