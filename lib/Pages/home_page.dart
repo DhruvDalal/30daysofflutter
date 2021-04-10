e50356920fbbb200241c29e4da63a493df48cbe0
DhruvDalal <dhruvdalal442002@gmail.com>
2021-04-11 00:01:00 +0530

    day13 completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,12 +1,35 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
 // ignore: must_be_immutable
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
-  String name = "Dhruv";
+
+  final String name = "Dhruv";
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    final catalogjson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogjson);
+    var productData = decodedData("products");
+    print(productData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dumyList = List.generate(20, (index) => catalogModel.items[0]);
