cb547da0843dee689a2ef93ce5adec01a87d5932
DhruvDalal <dhruvdalal442002@gmail.com>
2021-04-10 22:35:30 +0530

    day 10 done

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -7,11 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+// ignore: unused_import
 import 'package:integration_test/integration_test.dart';
 
 import 'package:flutter_catalog/main.dart' as app;
 
-void main() => run(_testMain);
+void main() => (_testMain);
 
 void _testMain() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
